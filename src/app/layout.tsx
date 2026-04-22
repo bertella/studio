65290baa43ae475a046fce259bcb983ede0c5352
lucianolattanzi358 <lucianolattanzi358@gmail.com>
2026-04-22 13:03:54 +0000

    Se implemento GTM x Anallytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { GoogleAnalytics } from '@next/third-parties/google';
+// Importamos el componente de GTM en lugar del de Analytics
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: 'DoctaData | Soluciones de Software y Experto en Low-Code',
@@ -19,15 +20,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        {/* Google Tag Manager en el Head (Mejor práctica para trackeo) */}
+        <GoogleTagManager gtmId="GTM-PB257DS7" />
       </head>
       <body className="font-body antialiased bg-background text-foreground selection:bg-accent selection:text-accent-foreground">
         {children}
         <Toaster />
-        {/* Google Analytics va aquí, justo antes de cerrar el body */}
-        <GoogleAnalytics gaId="G-T5J02SH72E" />
       </body>
     </html>
   );
 }
-
-
